57ce625b7e7cd7ccf7886ede9b47d505bf7df952
ikun <123@qq.com>
2020-08-27 12:11:43 +0800

    【知识点】label标签for属性实现自定义上传按钮样式

--- BigNews/admin/libs/http.js
+++ BigNews/admin/libs/http.js
@@ -7,7 +7,7 @@
         user_info:       baseURL + '/admin/user/info',//用户信息
         user_detail:     baseURL + '/admin/user/detail',//用户详情
         user_edit:       baseURL + '/admin/user/edit',//用户编辑
-        category_list:   baseURL + '/admin/category/list',//文章类别查询
+        category_list:   baseURL + '/admin/category/list'   ,//文章类别查询
         category_add:    baseURL + '/admin/category/add',//文章类别新增
         category_search: baseURL + '/admin/category/search',//文章类别搜索
         category_edit:   baseURL + '/admin/category/edit',//文章类别编辑
